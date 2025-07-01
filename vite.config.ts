4cc021d22ce9df07414fcc97d0d7db64a4878afb
Spc461 <spcbby123@gmail.com>
2025-07-01 13:50:49 +0200

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,21 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
+  build: {
+    rollupOptions: {
+      external: [
+        'date-fns',
+        'date-fns/locale/ar-SA',
+        'lucide-react'
+      ]
+    }
   },
+  optimizeDeps: {
+    include: [
+      'date-fns/format',
+      'date-fns/locale/ar-SA',
+      'lucide-react'
+    ],
+    exclude: ['lucide-react'] // Keep this if you specifically need to exclude it
+  }
 });
