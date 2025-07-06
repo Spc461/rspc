c1e18f5216a7e016994b8ffc50ae229a8956b37b
Spc461 <spcbby123@gmail.com>
2025-07-06 12:12:18 +0200

    Updated AdminDashboard.tsx

--- src/components/AdminDashboard.tsx
+++ src/components/AdminDashboard.tsx
@@ -316,7 +316,8 @@ const AdminDashboard = () => {
           >
             <Briefcase size={16} />
             طلبات التوظيف
-        <motion.button/>
+          </motion.button>
+          <motion.button
   whileHover={{ scale: 1.02 }}
   whileTap={{ scale: 0.98 }}
   onClick={() => setActiveTab('intern-applications')}
@@ -330,7 +331,7 @@ const AdminDashboard = () => {
   طلبات التدريب
 </motion.button>
 
-
+          
         </div>
       </div>
 
